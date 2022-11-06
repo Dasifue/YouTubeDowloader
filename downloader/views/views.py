3621c8640a0d97b5236fa8792a4b65999b3896f4
Dasifue <dasifue@gmail.com>
2022-11-06 13:32:00 +0600

    modified views: deleted global url, functions were sorted

---
+++ downloader/views/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+
+from pytube import YouTube
+
+from ..forms import VideoURLForm
+
+from .data import get_resolutions, video_data
+
+def videos_list(request):
+    form = VideoURLForm()
+    if request.method == "POST":
+        form = VideoURLForm(request.POST)
+        if form.is_valid():
+            url = request.POST.get("url")
+            yt = YouTube(url)
+            streams = yt.streams
+            resolutions = get_resolutions(streams=streams)
+
+            context = {
+                "video_data": video_data(url=url),
+                "form": form,
+                "resolutions": resolutions,
+                "audio": "audio"
+            }
+
+        return render(request=request, template_name="download.html", context=context)
+    return render(request=request, template_name="download.html", context={"form": form})
