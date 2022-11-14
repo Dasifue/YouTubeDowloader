f2c2d36066eb900e0f47d88000ab333848e1501c
Dasifue <dasifue@gmail.com>
2022-11-15 01:53:15 +0600

    updated views and templates

--- downloader/views/download.py
+++ downloader/views/download.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 
 from pytube import YouTube
@@ -8,18 +8,24 @@ import os
 
 def download_video(request, video_id, resolution):
     url = "https://www.youtube.com/watch?v=" + video_id
-    yt = YouTube(url)
-    st = yt.streams.filter(file_extension='mp4', type="video", resolution=resolution)
-    st.first().download()
-    return redirect(reverse_lazy("videos"))
+    try:
+        yt = YouTube(url)
+        st = yt.streams.filter(file_extension='mp4', type="video", resolution=resolution)
+        st.first().download()
+        return redirect(reverse_lazy("videos"))
+    except:
+        return render(request, "error.html")
 
 
 def download_audio(request, video_id):
     url = "https://www.youtube.com/watch?v=" + video_id
-    yt = YouTube(url)
-    st = yt.streams.filter(type='audio')
-    file = st.first().download()
-    base, exp = os.path.splitext(file)
-    new_file = base + '.mp3'
-    os.rename(file, new_file)
-    return redirect(reverse_lazy("videos"))
+    try:
+        yt = YouTube(url)
+        st = yt.streams.filter(type='audio')
+        file = st.first().download()
+        base, exp = os.path.splitext(file)
+        new_file = base + '.mp3'
+        os.rename(file, new_file)
+        return redirect(reverse_lazy("videos"))
+    except:
+        return render(request, "error.html")
